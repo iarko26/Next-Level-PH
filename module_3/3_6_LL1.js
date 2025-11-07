1290b9cfdeac0bcf338fb12c46c0db8ebeae09e9
merrimacksoft <94540571+iarko26@users.noreply.github.com>
2025-11-08 00:47:45 +0600

    update

--- module_3/3_6_LL1.js
+++ module_3/3_6_LL1.js
@@ -13,6 +13,16 @@ class LinkedList{
         this.length=0;
 
     }
+    _traverseIndex(index) {
+        let count=0;
+        let currentNode=this.head;
+        while(count!==index){
+            currentNode=currentNode.next;
+            count++;
+        }
+        return currentNode;
+
+    }
 //append end
  append(data){
     const newNode=new Node(data); //create a node
@@ -31,11 +41,76 @@ class LinkedList{
  }
 
 
-//   prepend() {}
+  prepend(data) {
+    const newNode=new Node(data);
+    if(this.head===null){
+        this.head=newNode;
+        this.tail=newNode;
+    }
+else{
+    newNode.next=this.head;
+    this.head=newNode;
+}
+this.length++;
+return this;
 
-//   insert() {}
 
-//   remove() {}
+  }
+
+  insert(index,value) {
+    if(index<0 ||index>this.length ){
+        return undefined;
+    }
+
+    if(index===0){
+       return this.prepend(value);
+    }
+    if(index===this.length){
+        return this.append(value);
+    }
+    
+    
+    const prevNode=this._traverseIndex(index-1);
+    const holdNode=prevNode.next;
+    const newNode=new Node(value);
+    prevNode.next=newNode;
+    newNode.next=holdNode;
+    this.length++;
+
+
+
+  }
+
+ removefirst(){
+    if(this.length===0){
+        return this.tail=null || undefined;
+    }
+    const removedfirstval=this.head.data;
+    this.head=this.head.next;
+    this.length--;
+    return removedfirstval;
+ }
+
+ removelast(){
+        if(this.length===0){
+        return this.tail=null || undefined;
+    }
+    const removelastval=this.tail.data;
+    if(this.length==1){
+        this.head=null;
+        this.tail=null;
+    }
+    else{
+        const prevNode=this._traverseIndex(this.length-2);
+        prevNode.next=null;
+        this.tail=prevNode;
+
+
+    }
+    this.length--;
+    return removelastval;
+
+ }
 
  print(){
     let temp=this.head;
@@ -58,8 +133,20 @@ class LinkedList{
 const ll=new LinkedList();
 ll.append(2);
 ll.append(3);
+ll.append(22);
+ll.append(30);
+ll.print();
+ll.prepend(1);
 ll.print();
 
+// ll.insert(3,100);
+// ll.print();
+
+// ll.removefirst();
+// ll.print();
+
+ll.removelast();
+ll.print();
 
 
 
